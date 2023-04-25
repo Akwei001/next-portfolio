6f19497ba0037b9d4e5464e5daf3a5d640ba9c64
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-04-25 21:43:54 +0100

    item added to resources

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
+const resources = [
+  {
+    name: 'pixabay',
+    description: 'An image bank website',
+    link: 'https://pixabay.com/',
+  },
+  {
+    name: '',
+    description: '',
+    link: '',
+  },
+  {
+    name: '',
+    description: '',
+    link: '',
+  },
+  {
+    name: '',
+    description: '',
+    link: '',
+  },
+  {
+    name: '',
+    description: '',
+    link: '',
+  },
+  {
+    name: '',
+    description: '',
+    link: '',
+  },
+];
+
 const Resources = () => {
   return <div>Links to resources will go here</div>;
 };
